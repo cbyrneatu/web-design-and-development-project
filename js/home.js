2dc40812b189d7066835e0e28addb73431fdb602
cbyrneatu <S00250789@atu.ie>
2024-11-25 10:33:15 +0000

    Extract product card creation into another function

--- js/home.js
+++ js/home.js
@@ -37,33 +37,38 @@ const productsArea = document.getElementById("products-area");
 
 function insertProducts() {
     for (const product of products) {
-        const container = document.createElement("div");
-        container.className = "col"
-
-        const card = document.createElement("div");
-        card.className = "card";
-
-        const body = document.createElement("div");
-        body.className = "card-body";
-
-        const image = document.createElement("img");
-        image.src = "https://placehold.co/600x400";
-        image.className = "card-img-top";
-
-        const title = document.createElement("h5");
-        title.textContent = product.name;
-
-        const description = document.createElement("p");
-        description.textContent = product.description;
-
-        body.appendChild(title);
-        body.appendChild(description);
-        card.appendChild(image);
-        card.appendChild(body);
-        container.appendChild(card);
-
-        productsArea.appendChild(container);
+        const card = createProductCard(product);
+        productsArea.appendChild(card);
     }
+}
+
+function createProductCard(product) {
+    const container = document.createElement("div");
+    container.className = "col"
+
+    const card = document.createElement("div");
+    card.className = "card";
+
+    const body = document.createElement("div");
+    body.className = "card-body";
+
+    const image = document.createElement("img");
+    image.src = "https://placehold.co/600x400";
+    image.className = "card-img-top";
+
+    const title = document.createElement("h5");
+    title.textContent = product.name;
+
+    const description = document.createElement("p");
+    description.textContent = product.description;
+
+    body.appendChild(title);
+    body.appendChild(description);
+    card.appendChild(image);
+    card.appendChild(body);
+    container.appendChild(card);
+
+    return container;
 }
 
 insertProducts();
